ab71c7fccaf0137ce2dc3b3b7482a99c010a70b3
h4rasho <h4rasho@gmail.com>
2023-05-28 13:02:10 -0400

    fix icon name props

--- src/components/icons/PlusIcon.tsx
+++ src/components/icons/PlusIcon.tsx
@@ -1,7 +1,7 @@
 export function PlusIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48">
-      <g fill="none" fill-rule="evenodd">
+      <g fill="none" fillRule="evenodd">
         <path
           stroke="#EEE"
           d="M24 47.5c12.979 0 23.5-10.521 23.5-23.5S36.979.5 24 .5.5 11.021.5 24 11.021 47.5 24 47.5Z"
