cd487a6a806c7596ecfd28fee8733c04fdc84ab2
h4rasho <h4rasho@gmail.com>
2023-12-25 17:07:49 -0300

    ui pagination

--- src/app/results/components/Pagination.tsx
+++ src/app/results/components/Pagination.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import {ChevronLeftIcon} from '@/components/icons/ChevronLeftIcon'
+import {ChevronRightIcon} from '@/components/icons/ChevronRightIcon'
 import {usePathname, useRouter, useSearchParams} from 'next/navigation'
 
 export function Pagination({
@@ -18,20 +20,33 @@ export function Pagination({
   }
 
   return (
-    <div className="flex justify-center gap-3">
+    <div className="flex items-center  mt-4 w-80 justify-around m-auto">
       <button
-        className="bg-gray-100 px-4 py-2 rounded-md"
+        className="flex items-center gap-1 bg-gray-100 rounded-md text-[#3483fa]"
         disabled={currentPage === 1}
         onClick={() => onPageChange(currentPage - 1)}
       >
+        <span>
+          <ChevronLeftIcon className="w-4" />
+        </span>
         Anterior
       </button>
+      <div className="flex items-center gap-2 text-[#737373]">
+        <p className=" bg-[#e4e4e4] py-2 px-3 rounded-sm">{currentPage}</p>
+        <p>
+          <span> de </span>
+          {totalPages}
+        </p>
+      </div>
       <button
-        className="bg-gray-100 px-4 py-2 rounded-md"
+        className="flex items-center gap-1 bg-gray-100  rounded-md text-[#3483fa]"
         disabled={currentPage === totalPages}
         onClick={() => onPageChange(currentPage + 1)}
       >
         Siguiente
+        <span>
+          <ChevronRightIcon className="w-4" />
+        </span>
       </button>
     </div>
   )
