24f88dafab235f1560fa74140ed6b3525d72e0b7
h4rasho <h4rasho@gmail.com>
2024-01-01 19:19:13 -0300

    remove no used import

--- src/app/product/components/Product.List.tsx
+++ src/app/product/components/Product.List.tsx
@@ -1,4 +1,3 @@
-import {getProducts} from '@/app/product/service/productService'
 import {ProductCard} from './ProductCard'
 import {IProduct} from '../types/product'
 
@@ -6,7 +5,6 @@ export async function ProductList({products}: {products: IProduct[]}) {
   return (
     <ul className="flex gap-2">
       {products?.map(product => {
-        /* @ts-expect-error Server Component */
         return <ProductCard key={product.id} product={product} />
       })}
     </ul>
