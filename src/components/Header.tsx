de9883e922ade9396e8ef8606d544463caad8248
h4rasho <h4rasho@gmail.com>
2024-01-02 21:37:31 -0300

    add link to home page when user clicks on logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,26 @@
-import Image from "next/image";
+import Image from 'next/image'
+import Link from 'next/link'
 
-import logo from "../../public/logo.png";
-import disneyPromo from "../../public/disney-promo.webp";
-import { LocationIcon } from "./icons/LocationIcon";
-import { Nav } from "./Nav";
-import UserOptions from "./UserOptions";
-import { Search } from "./Search";
+import logo from '../../public/logo.png'
+import disneyPromo from '../../public/disney-promo.webp'
+import {LocationIcon} from './icons/LocationIcon'
+import {Nav} from './Nav'
+import UserOptions from './UserOptions'
+import {Search} from './Search'
 
 export function Header() {
   return (
     <header className="flex font-light text-sm flex-col items-center bg-[#fff159] w-full p-2">
       <section className="flex justify-between max-w-screen-xl w-full">
         <div className="flex flex-col gap-2">
-          <Image src={logo} width={134} height={34} alt="Mercado Libre logo" />
+          <Link href="/">
+            <Image
+              src={logo}
+              width={134}
+              height={34}
+              alt="Mercado Libre logo"
+            />
+          </Link>
           <button className="text-sm flex text-gray-600 font-light gap-1">
             <LocationIcon />
             <div className="flex flex-col">
@@ -36,5 +44,5 @@ export function Header() {
         </div>
       </section>
     </header>
-  );
+  )
 }
