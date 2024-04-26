1daad59cf866b34bd48d0f3cc0f8c9dec34938f1
h4rasho <h4rasho@gmail.com>
2024-04-25 21:58:12 -0400

    fix ui product cart

--- src/app/product/components/ProductCard.tsx
+++ src/app/product/components/ProductCard.tsx
@@ -1,19 +1,19 @@
 import Link from 'next/link'
-import {IProduct} from '../types/product'
+import { IProduct } from '../types/product'
 
-export async function ProductCard({product}: {product: IProduct}) {
+export async function ProductCard({ product }: { product: IProduct }) {
   return (
-    <article className="bg-white w-full">
+    <article className="bg-white max-w-44s w-full">
       <Link href={`/product/${product.id}`} className="hover:text-[#3483fa]">
         <img
-          className="aspect-square object-contain mx-auto"
+          className="aspect-square object-contain mx-auto max-w-44"
           src={product.pictures?.at(0)}
           width={150}
           height={150}
           alt={product.description || product.title}
         />
         <footer className="text-sm font-regular border-gray-100">
-          <p className="overflow-hidden line-clamp-2">{product.description}</p>
+          <p className="overflow-hidden line-clamp-2">{product.title}</p>
           <p className="text-2xl text-black ">{`${Number(
             product.price
           ).toLocaleString('es-Cl', {
